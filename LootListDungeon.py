8fa7b41453bba906fe19b84b62c16113ef27ed57
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-08-18 12:39:05 -0700

    Add files via upload

---
+++ LootListDungeon.py
@@ -0,0 +1,37 @@
+#weapons and armor for dungeon game, will convert to C# later, or i could put it into a csv file. idk
+
+
+
+#{name: [buffs]}
+trophies = {#its no buff for now, some can be just aesthetic though
+    "cup of demons blood": [None],
+    "orc shahman's head": [None]
+}
+#{name: value}
+coins = {"copper": 1,"silver": 5, "gold": 10}
+#{name: [durability, damage dealt, range, [attributes ("enchants will be  random")]]}
+weapons = {
+        "axe": [500, 10, 2, [None]],
+        "sword": [300, 15, 1, [None]],
+        "spear": [400, 8, 4, [None]]
+        }#{name: [durability, damage dealt, range, rate of fire (per minute),[attributes ("enchants will be  random")]]}
+rangedWeapons = {
+        "bow": [500, 6, 10, 30 ,[None]],
+        "crossbow": [300, 15, 1, 20 ,[None]],
+        }
+#{name: [durabilty, armor protection, [attributes ("enchants will be  random")]]}
+helmet = {
+    
+}
+#{name: [durabilty, armor protection, [attributes ("enchants will be  random")]]}
+chesplate = {
+    
+}
+#{name: [durabilty, armor protection, [attributes ("enchants will be  random")]]}
+legplate = {
+    
+}
+#{name: [durabilty, armor protection, [attributes ("enchants will be  random")]]}
+shoes = {
+    
+}
