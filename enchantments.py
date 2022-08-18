1ff8417636b4c7de34e27f8b37d0abc9f2a24f5c
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-08-18 16:15:19 -0700

    Add files via upload

---
+++ enchantments.py
@@ -0,0 +1,23 @@
+#the numbers are just place holders 
+weaponEnchants = {#it'll work when its in c#, this is just for concept and ideas
+    "sharper": "damage += 1",
+    "aware": "range += 1",
+    "hardened": "durability += 300"
+}
+
+rangedWeaponEnchants = {
+    "pointy": "damage += 1",
+    "piercing": "shoot thorugh multiple enemies"
+}
+
+allArmorEnchants = {
+    "favored": "loot rarity += 1",
+    "plentiful": "loot amount += 1",
+    "hardened": "durability += 300",
+}
+
+legendaryEnchants = {
+    "leech": "health steal from killed enemies",
+    "killshot": "one shot all basic enemies",
+    "one of us": "invisable to enemies"#VERY RARE
+}
