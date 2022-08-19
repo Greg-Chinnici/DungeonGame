e3c5504a3b7c0d92743d30ab88f4f394abd5b9fa
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-08-18 19:36:57 -0700

    Create mobTemplate.py

---
+++ mobTemplate.py
@@ -0,0 +1,8 @@
+class mob():
+  def __init__(self, name, theme, type, health, weapon):
+    self.name = name
+    self.theme = theme
+    self.type = type
+    self.health = health
+    self.weapon = weapon
+    
