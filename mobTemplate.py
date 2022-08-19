a8645aacf97fea6398545bf6e44a1324f2e5afa0
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-08-18 22:20:03 -0700

    Update mobTemplate.py

--- mobTemplate.py
+++ mobTemplate.py
@@ -1,3 +1,11 @@
+class weapon():
+  def __init__(self,w):
+    self.name = w[0]
+    self.durability = w[1]
+    self.damage = w[2]
+    self.range = w[3]
+    self.attributes = w[4]
+ 
 class mob():
   def __init__(self, name, theme, type, health, weapon):
     self.name = name
