dab774b0bf7435f47c03a222bad7e304fdd4b1d5
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-08-19 19:59:49 -0700

    Create ShopItems.py

---
+++ ShopItems.py
@@ -0,0 +1,6 @@
+#{name: boost} , names are not finalized
+DungeonBoosts = {
+    "Better Loot" : ["loot rarity ++"],
+    "More Loot": ["loot cnt ++"],
+    "Strong": ["weakens all enemies (even bosses)"]
+}
