ab0b2367a417a5389883d2b751ab7aae785d3b24
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-08-20 22:09:55 -0700

    Add files via upload

---
+++ DungeonGameLogic.py
@@ -0,0 +1,18 @@
+#gameloop
+'''
+roosm will be stored after the player leves them so they will retain thier state if the player wants to go back 
+'''
+
+#i might want to make it change based on a radius from  the  spawn room
+health = 20
+roomsEntered = 0 #this will be the seed for the increased loot rarity
+while health > 0:
+    if isInExitDoor(player):
+        makeNewRoom(roomsEntered)#genreates a new room and teleports the player into it, also makes chest and other room addOns 
+        roomsEntered += 1
+    
+    if getEscapeEagle(player):
+        spawnEagle() #escape eaglew spawns and picks up the player, there is a 30 second delay so you cant combat log
+#when rhe player dies
+
+goBackToBase(player) #teleports the player to home base if they die, and clears inventory
