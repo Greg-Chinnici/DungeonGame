591b94eb5972b31371d8079ac229386379cfd300
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-08-22 06:03:32 -0700

    Update LootListDungeon.py

--- LootListDungeon.py
+++ LootListDungeon.py
@@ -14,7 +14,7 @@ bossKeys = {#keys are very rare drops
     "Neptune": ["any water type mob"],
     "Wendigo": ["mobs killed at night / dark places"],
     "Yeti": ["any ice type mob"],
-    "Loviatar": ["water type mobs],
+    "Loviatar": ["water type mobs"],
     "Ogre King": ["chest loot"]
 }
 
@@ -31,21 +31,32 @@ rangedWeapons = {
         "bow": [500, 6, 10, 30 ,[None]],
         "crossbow": [300, 15, 1, 20 ,[None]],
         }
+
+
+#these will be combined into an array of player armor and thier stats will be combined
 #{name: [durabilty, armor protection, [attributes ("enchants will be  random")]]}
 helmet = {
-    
+    "leather helm": [1000, 1, [None]],
+    "chainmail helm": [1500, 2, [None]],
+    "knights helm": [3000, 4, [None]]
 }
 #{name: [durabilty, armor protection, [attributes ("enchants will be  random")]]}
 chesplate = {
-    
+    "leather chesplate": [1000, 3, [None]],
+    "chainmail chesplate": [1500, 4, [None]],
+    "knights chesplate": [3000, 7, [None]]
 }
 #{name: [durabilty, armor protection, [attributes ("enchants will be  random")]]}
 legplate = {
-    
+    "leather legplate": [1000, 2, [None]],
+    "chainmail braves": [1500, 3, [None]],
+    "knights greaves": [3000, 6, [None]]
 }
 #{name: [durabilty, armor protection, [attributes ("enchants will be  random")]]}
 shoes = {
-    
+    "leather shoes": [1000, 1, [None]],
+    "chainmail shoes": [1500, 3, [None]],
+    "knights shoes": [3000, 5, [None]]
 }
 #{name: [effects]}
 potions = {
