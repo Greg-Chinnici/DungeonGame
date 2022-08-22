383805e1159b530aef548f4f42ad92c6dc92ce9f
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-08-21 20:53:31 -0700

    Update ShopItems.py

--- ShopItems.py
+++ ShopItems.py
@@ -1,6 +1,12 @@
+#! havent made any prices yet
+
 #{name: boost} , names are not finalized
 DungeonBoosts = {
     "Better Loot" : ["loot rarity ++"],
     "More Loot": ["loot cnt ++"],
     "Strong": ["weakens all enemies (even bosses)"]
+}
+DundgeonRunnerKits = {
+    'Starter kit' = [[DungeonLootList[weapons][random.choice(range(3))]] , [DungeonLootList[armorKits][random.choice(range(3))]] #pseudocode for picking the a random item from the weaapon list, number in the range will be from the rarity
+
 }
