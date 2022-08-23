86c61483dcc1187b9fdbc84ff8bf55399468a1ac
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-08-23 00:59:44 -0700

    Update LootListDungeon.py

--- LootListDungeon.py
+++ LootListDungeon.py
@@ -21,11 +21,29 @@ bossKeys = {#keys are very rare drops
 #{name: value}
 coins = {"copper": 1,"silver": 5, "gold": 10}
 #! loot will get better in the lists, higher index == stronger item
-#{name: [durability, damage dealt, range, [attributes ("enchants will be  random")]]}
+#{name: [name, durability, damage, range, attackSpeed, type, attributes ("enchants will be  random")]}
 weapons = {
-        "axe": [500, 10, 2, [None]],
-        "sword": [300, 15, 1, [None]],
-        "spear": [400, 8, 4, [None]]
+    "Iron knife": [400 , 12 ,1 , 2 , "dagger", [None]],
+    "Bronze knife": [500 , 16 ,1 , 3 , "dagger", [None]],
+    "Steel knife": [600 , 20 ,2 , 4 , "dagger", [None]],
+    "Iron Longsword": [500 , 14 ,2 , 0.5 , "longsword", [None]],
+    "Bronze Longsword": [600 , 18 ,3 , 0.75 , "longsword", [None]],
+    "Steel Longsword": [700 , 22 ,4 , 1 , "longsword", [None]],
+    "Stone Axe": [500 , 15 ,2 , 1 , "handaxe", [None]],
+    "Iron Axe": [700 , 18 ,2 , 1.25 , "handaxe", [None]],
+    "Steel Axe": [800 , 21 ,2 , 1.5 , "handaxe", [None]],
+    "Stone BattleaAxe": [700 , 18 ,2 , 0.33 , "battleaxe", [None]],
+    "Iron BattleaAxe": [800 , 21 ,2 , 0.33 , "battleaxe", [None]],
+    "Steel BattleaAxe": [950 , 23 ,3 , 0.33 , "battleaxe", [None]],
+    "Iron Gauntlet": [400 , 20 ,1 , 0.5 , "gauntlet", [None]],
+    "Bronze Gauntlet": [500 , 22 ,1 , 0.75 , "gauntlet", [None]],
+    "Steel Gauntlet": [600 , 25 ,1 , 1 , "gauntlet", [None]],
+    "Iron Mace": [500 , 10 ,1 , 1.33 , "mace", [None]],
+    "Bronze Mace": [650 , 15 ,1 , 1.66 , "mace", [None]],
+    "Steel Mace": [800 , 21 ,1 , 2 , "mace", [None]],
+    "Iron Spear": [400 , 13 ,3 , 1 , "spear", [None]],
+    "Steel Spear": [600 , 17 ,4 , 1.33 , "spear", [None]],
+    "Diamond Spear": [800 , 22 ,5 , 1.66 , "spear", [None]]
         }#{name: [durability, arrow type , damage dealt, range, rate of fire (per minute),[attributes ("enchants will be  random")]]}
 rangedWeapons = {
         "bow": [500, [None] ,6, 10, 30 ,[None]],
