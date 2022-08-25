dc7ae1a2a592ec79f7581dc5ba46bff100486a45
Greg-Chinnici <chinnici@chapman.edu>
2022-08-24 21:02:35 -0700

    added a class type Key for room keys

--- MobDropLootTable.py
+++ MobDropLootTable.py
@@ -36,29 +36,48 @@ def sortLists():
 
 coins = {"copper": 1,"silver": 5, "gold": 10}
 GeneralLootTable = [coins] #isnt effected by type or theme
+class key():
+    def __init__(self , name , opensDoor , ):
+        self.name = name
+        self.opensDoor = opensDoor
+    def printInfo(self):
+        print(f"this key will open the {self.opensDoor}")
+
+wedigoKey = key("Wendigo Key" , "Wendigo Forest")
+hadesKey = key('Hades Key' , "depths of hell")
+plutoKey = key('Pluto Key' , 'underworld lair')
+helKey = key("Hel Key" , 'cabin of death')
+apophisKey = key('Apophis Key' , "Apophis\' Snake Pit")
+yetiKey = key("Yeti Key" , "Yeti Cave")
+loviatarKey = key('Loviatar Key' , "Loviatar\'s Layer")
+neptuneKey = key('Neptune Key' , "Atlantis Vault")#neptune isnt from atlantis but idc, cool name
+aresKey = key('Ares Key' , "Ares\' War Room")
+freyaKey = key('Freya Key' , 'Freya\'s Mansion')
+odinKey = key('Odin Key' , 'Odin\'s Throne Room' )
 
 TimeBasedLootTable = {
     'day' : [],
-    'night': ["Wendigo key"]
+    'night': [wedigoKey]
 }
 
 TypeLootTable = {
-    'death': [],
+    'death': [hadesKey , plutoKey , helKey, apophisKey],
     'earth': [],
-    'fire': [],
-    'ice': ['Yeti key'],
+    'fire': [freyaKey],
+    'ice': [yetiKey],
     'lightning': [],
-    'magic': [],
+    'magic': [odinKey],
     'sand': [],
-    'water': ['Loviatar key', 'Neptune key']
+    'water': [loviatarKey, neptuneKey]
 }
 ThemeLootTable = {
+    'egyptian': [apophisKey],
     'folklore': [],
-    'greek': ['Ares key'],
+    'greek': [aresKey , hadesKey],
     'historical': [],
     'medival': [],
     'pirate': [],
     'popCulture': [],
-    'roman': [],
-    'viking': []
+    'roman': [plutoKey],
+    'viking': [helKey , odinKey]
 }
