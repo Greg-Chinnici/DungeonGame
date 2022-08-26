28f85e2269566e12974144ca733bfa0d87a9b190
Greg-Chinnici <chinnici@chapman.edu>
2022-08-26 10:55:56 -0700

    fixing enchants

--- Enchantments.py
+++ Enchantments.py
@@ -1,10 +1,24 @@
 #the numbers are just place holders 
+class weapon():
+    def __init__(self, name, durability, damage, range, attackSpeed, type, attributes = None):
+        self.name = name
+        self.durability = durability
+        self.damage = damage
+        self.range = int(range)
+        self.attackSpeed = attackSpeed #per second
+        self.type = type
+        self.attributes = attributes
+    def printInfo(self):
+        print(f"\"{self.name}\": [{self.durability} , {self.damage} ,{self.range} , {self.attackSpeed} , \"{self.type}\", {self.attributes}]") #prints info in an easy dictionay form
+
+
+
 weaponEnchants = {#it'll work when its in c#, this is just for concept and ideas
-    "sharper": "damage += 1",
-    "aware": "range += 1",
-    "hardened": "durability += 300",
-    "haste": "attack speed += 1",
-    "vorpal": "higher crit chance"
+    "sharper": weapon.damage = weapon.damage * 1.5,#? will have multiple levels of sharpness
+    "aware": weapon.range += 1,
+    "hardened": weapon.durability += 300,
+    "haste": weapon.attackSpeed *= 1.5, #? multple levls too?
+    "vorpal": "higher crit chance" # i think crit chance should be tied to the player, armor can also increase this
 }
 
 rangedWeaponEnchants = {
