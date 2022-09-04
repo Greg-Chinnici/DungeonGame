ec8e99eca1ce008db89f8065cd25987e888ab442
Greg-Chinnici <chinnici@chapman.edu>
2022-09-04 01:02:55 -0700

    added a sum function

--- cellularAtomata.py
+++ cellularAtomata.py
@@ -1,38 +1,45 @@
 import random
 import math
 
-height = 4
-width = 4
-grid = [range(height)*[range(width) * [0]]]
+height = 5
+width = 5
+grid = [[0] * width] * height
 
 def firstFill():
-    for lineIndex , line in enumerate(grid):
-        for itemIndex , item in enumerate(line):
-            grid[lineIndex][itemIndex] = random.randint(0,1)
+    for rowIndex , row in enumerate(grid):
+        for itemIndex , item in enumerate(row):
+            grid[rowIndex][itemIndex] = random.randint(0,1)
 
-def getWeight(lineIndex, itemIndex):
+def sumNum(listOfNums):
+    sum = 0
+    for n in listOfNums:
+        sum += n
+    return sum
+
+def getWeight(lineIndex, itemIndex, grid):
     #check the 8 surrouding items for their fill, 1 == wall  0 == floor
-    weight = math.sum(grid[lineIndex-1][itemIndex-1], grid[lineIndex-1][itemIndex], grid[lineIndex-1][itemIndex+1], grid[lineIndex][itemIndex-1], grid[lineIndex][itemIndex+1], grid[lineIndex+1][itemIndex-1], grid[lineIndex+1][itemIndex],  grid[lineIndex][itemIndex+1])
+    weight = sumNum([grid[lineIndex-1][itemIndex-1], grid[lineIndex-1][itemIndex], grid[lineIndex-1][itemIndex+1], grid[lineIndex][itemIndex-1], grid[lineIndex][itemIndex+1], grid[lineIndex+1][itemIndex-1], grid[lineIndex+1][itemIndex],  grid[lineIndex][itemIndex+1]])
     weight /= 4 #normalizes the average
-
+    print(weight)
     return weight
 
-def newPass():
-    workingGrid = [range(height)*[range(width) * [0]]]
+def newPass(grid):
+    workingGrid = [[0] * width] * height
 
     for lineIndex , line in enumerate(grid):
         for itemIndex , item in enumerate(line):
             if lineIndex == 0 or itemIndex == 0 or lineIndex == height-1 or itemIndex == width-1:
-                pass
-            else:
-                workingGrid[lineIndex][itemIndex] = 1 if getWeight(lineIndex, itemIndex) > 1 else 0
-    
+                print(item)
+            else: #! not working
+                workingGrid[lineIndex][itemIndex] = 1 if getWeight(lineIndex, itemIndex, grid) < 1 else 0
     return workingGrid
 
 
-
 firstFill()
-passes = 3
+for line in grid:
+        print(line , "\n")
+
+passes = 1
 for i in range(passes):
-    grid = newPass()
+    grid = newPass(grid)
     print(grid)
