ab8edd898ec38730f2118178b0c82128f7d50f81
Greg-Chinnici <chinnici@chapman.edu>
2022-09-10 01:32:49 -0700

    added a dispaly method

--- cellularAtomata.py
+++ cellularAtomata.py
@@ -4,10 +4,13 @@ import math
 height = 5
 width = 5
 grid = [[0] * width] * height
-
 def firstFill():
+    cnt = 0
+
     for rowIndex , row in enumerate(grid):
         for itemIndex , item in enumerate(row):
+            #print(f"{cnt}: {rowIndex} , {itemIndex}")
+            cnt += 1
             grid[rowIndex][itemIndex] = random.randint(0,1)
 
 def sumNum(listOfNums):
@@ -20,7 +23,7 @@ def getWeight(lineIndex, itemIndex, grid):
     #check the 8 surrouding items for their fill, 1 == wall  0 == floor
     weight = sumNum([grid[lineIndex-1][itemIndex-1], grid[lineIndex-1][itemIndex], grid[lineIndex-1][itemIndex+1], grid[lineIndex][itemIndex-1], grid[lineIndex][itemIndex+1], grid[lineIndex+1][itemIndex-1], grid[lineIndex+1][itemIndex],  grid[lineIndex][itemIndex+1]])
     weight /= 4 #normalizes the average
-    print(weight)
+    #print(weight)
     return weight
 
 def newPass(grid):
@@ -29,17 +32,24 @@ def newPass(grid):
     for lineIndex , line in enumerate(grid):
         for itemIndex , item in enumerate(line):
             if lineIndex == 0 or itemIndex == 0 or lineIndex == height-1 or itemIndex == width-1:
-                print(item)
+                pass
             else: #! not working
                 workingGrid[lineIndex][itemIndex] = 1 if getWeight(lineIndex, itemIndex, grid) < 1 else 0
     return workingGrid
 
+def display(grid):
+    for row in grid:
+        for num in row:
+            if num == 1:
+                print("#" , end = "")
+            else:
+                print(" " , end = "")
+        print("")
 
 firstFill()
-for line in grid:
-        print(line , "\n")
+display(grid)
 
-passes = 1
+passes = 3
 for i in range(passes):
     grid = newPass(grid)
-    print(grid)
+    display(grid)
